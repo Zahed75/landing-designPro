bb728b88f05aa53ac3cccd252dd27894d6ab5be0
Your Name <user@example.com>
2025-06-27 15:27:22 +0600

    Contact section added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { BlogPage } from './blog.page';
+import { ContactPage } from './contact.page';
+import { HomePage } from './home.page';
 
 export const routes: Routes = [
+  { path: '', component: HomePage },
   { path: 'blog', component: BlogPage },
+  { path: 'contact', component: ContactPage },
   // ...other routes
 ];
