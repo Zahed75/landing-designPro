c2a71db47c6bb57c65d0e8cca6441ee7cd922cd8
Chuskibot <rouhithmini1@gmail.com>
2025-07-22 20:09:29 +0600

    section added

--- src/app/our-work.page.ts
+++ src/app/our-work.page.ts
@@ -36,6 +36,24 @@ export class OurWorkPage {
       ]
     },
     {
+      title: 'Email Design',
+      description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
+      images: [
+        '/assets/images/08182022_Integrated-Campaign-Mock-Ups_V2_2-03-768x768.webp',
+        '/assets/images/7-ElevenMockup-768x432.png',
+        '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
+      ]
+    },
+    {
+      title: 'Web Design',
+      description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
+      images: [
+        '/assets/images/08182022_Integrated-Campaign-Mock-Ups_V2_2-03-768x768.webp',
+        '/assets/images/7-ElevenMockup-768x432.png',
+        '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
+      ]
+    },
+    {
       title: 'Integrated Campaign Designs',
       description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
       images: [
@@ -43,6 +61,64 @@ export class OurWorkPage {
         '/assets/images/7-ElevenMockup-768x432.png',
         '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
       ]
-    }
+    },
+    {
+      title: 'Packaging & Merchandise Design',
+      description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
+      images: [
+        '/assets/images/08182022_Integrated-Campaign-Mock-Ups_V2_2-03-768x768.webp',
+        '/assets/images/7-ElevenMockup-768x432.png',
+        '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
+      ]
+    },
+    {
+      title: 'Print Design',
+      description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
+      images: [
+        '/assets/images/08182022_Integrated-Campaign-Mock-Ups_V2_2-03-768x768.webp',
+        '/assets/images/7-ElevenMockup-768x432.png',
+        '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
+      ]
+    },
+    {
+      title: 'Branding & Logo Design',
+      description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
+      images: [
+        '/assets/images/08182022_Integrated-Campaign-Mock-Ups_V2_2-03-768x768.webp',
+        '/assets/images/7-ElevenMockup-768x432.png',
+        '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
+      ]
+    },
+    {
+      title: 'eBook & Digital Report Design',
+      description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
+      images: [
+        '/assets/images/08182022_Integrated-Campaign-Mock-Ups_V2_2-03-768x768.webp',
+        '/assets/images/7-ElevenMockup-768x432.png',
+        '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
+      ]
+    },
+    {
+      title: 'Custom Illustrations',
+      description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
+      images: [
+        '/assets/images/08182022_Integrated-Campaign-Mock-Ups_V2_2-03-768x768.webp',
+        '/assets/images/7-ElevenMockup-768x432.png',
+        '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
+      ]
+    },
+    {
+      title: 'Infographics',
+      description: 'Comprehensive campaign visuals across web, social, and print. Consistent branding and creative storytelling for maximum impact.',
+      images: [
+        '/assets/images/08182022_Integrated-Campaign-Mock-Ups_V2_2-03-768x768.webp',
+        '/assets/images/7-ElevenMockup-768x432.png',
+        '/assets/images/1093520_Mockup-Work-SamplesSocial5_060921.jpg'
+      ]
+    },
+    
+
+
+    
   ];
 }
