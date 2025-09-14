15527187a680e57da9d40aaa0c566d219f05c811
Zahed75 <zahedhasan.cs@gmail.com>
2025-09-15 02:31:49 +0600

    our work css fixed

--- src/enviornments/enviornment.ts
+++ src/enviornments/enviornment.ts
@@ -12,5 +12,6 @@ export const environment = {
   production: false,
   apiBaseUrl: 'https://api.designpro.qa', 
   // apiBaseUrl: ' http://127.0.0.1:8000', 
-  wsHost: 'localhost:8000'
+  // wsHost: 'localhost:8000'
+  wsHost: 'https://api.designpro.qa'
 };
