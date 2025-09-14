933336f8991db277d40962b3b0f92e72e9040888
Zahed75 <zahedhasan.cs@gmail.com>
2025-09-15 03:03:34 +0600

    our work css fixed

--- src/enviornments/enviornment.ts
+++ src/enviornments/enviornment.ts
@@ -8,10 +8,19 @@
 
 // Development environment configuration
 
+// export const environment = {
+//   production: false,
+//   apiBaseUrl: 'https://api.designpro.qa', 
+//   // apiBaseUrl: ' http://127.0.0.1:8000', 
+//   // wsHost: 'localhost:8000'
+//   wsHost: 'https://api.designpro.qa'
+// };
+
+
+
+
+
 export const environment = {
-  production: false,
-  apiBaseUrl: 'https://api.designpro.qa', 
-  // apiBaseUrl: ' http://127.0.0.1:8000', 
-  // wsHost: 'localhost:8000'
-  wsHost: 'https://api.designpro.qa'
+  production: true,
+  apiBaseUrl: 'https://api.designpro.qa' 
 };
